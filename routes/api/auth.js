d9ec44efacdce41599960384119e6c7485c4ae3d
m1g <miguel.malcolm@spatialnetworks.com>
2019-10-30 17:30:51 -0400

    adds user auth. login and logout

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,6 +8,19 @@ const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
+// @route    GET api/auth
+// @desc     Test route
+// @access   Public
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 // @route     POST api/auth
 // @desc      Authenticate user & get token
 // @access    Public
