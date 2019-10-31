4feb49dbe8db4bd5642f9d116af00ff56b5a7175
m1g <miguel.malcolm@spatialnetworks.com>
2019-10-31 15:44:48 -0400

    adds create profile feature

---
+++ client/src/components/profile-forms/CreateProfile.js
@@ -0,0 +1,187 @@
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
+
+const CreateProfile = ({ createProfile, history }) => {
+  const [formData, setFormData] = useState({
+    bio: '',
+    location: '',
+    status: '',
+    destinations: '',
+    youtube: '',
+    twitter: '',
+    facebook: '',
+    linkedin: '',
+    instagram: ''
+  });
+
+  const [displaySocialInputs, toggleSocialInputs] = useState(false);
+
+  const {
+    bio,
+    location,
+    status,
+    destinations,
+    youtube,
+    twitter,
+    facebook,
+    linkedin,
+    instagram
+  } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
+
+  return (
+    <>
+      <h1 className="large text-primary">Create Your Profile</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Let's get some information to make your
+        profile stand out
+      </p>
+      <small>* = required field</small>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <select name="status" value={status} onChange={e => onChange(e)}>
+            <option value="0">* Select Travel Status</option>
+            <option value="This Month">Traveling this month</option>
+            <option value="Next Month">Traveling next month</option>
+            <option value="Next Few Months">
+              Traveling in the next few months
+            </option>
+            <option value="Next Year">Traveling next year</option>
+            <option value="Future">Planning a distant future trip</option>
+          </select>
+          <small className="form-text">
+            Give us an idea of when you want to travel
+          </small>
+        </div>
+        <div className="form-group">
+          <textarea
+            placeholder="A short bio of yourself"
+            name="bio"
+            value={bio}
+            onChange={e => onChange(e)}
+          ></textarea>
+          <small className="form-text">Tell us a little about yourself</small>
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Living in"
+            name="location"
+            value={location}
+            onChange={e => onChange(e)}
+          />
+          <small className="form-text">
+            City & state suggested (eg. Brooklyn, NY)
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Destinations"
+            name="destinations"
+            value={destinations}
+            onChange={e => onChange(e)}
+          />
+          <small className="form-text">
+            Please use comma separated values (eg. Los Angeles,New
+            York,Miami,Vegas)
+          </small>
+        </div>
+
+        <div className="my-2">
+          <button
+            onClick={() => toggleSocialInputs(!displaySocialInputs)}
+            type="button"
+            className="btn btn-light"
+          >
+            Add Social Network Links
+          </button>
+          <span>Optional</span>
+        </div>
+
+        {displaySocialInputs && (
+          <>
+            <div className="form-group social-input">
+              <i className="fab fa-twitter fa-2x"></i>
+              <input
+                type="text"
+                placeholder="Twitter URL"
+                name="twitter"
+                value={twitter}
+                onChange={e => onChange(e)}
+              />
+            </div>
+
+            <div className="form-group social-input">
+              <i className="fab fa-facebook fa-2x"></i>
+              <input
+                type="text"
+                placeholder="Facebook URL"
+                name="facebook"
+                value={facebook}
+                onChange={e => onChange(e)}
+              />
+            </div>
+
+            <div className="form-group social-input">
+              <i className="fab fa-youtube fa-2x"></i>
+              <input
+                type="text"
+                placeholder="YouTube URL"
+                name="youtube"
+                value={youtube}
+                onChange={e => onChange(e)}
+              />
+            </div>
+
+            <div className="form-group social-input">
+              <i className="fab fa-linkedin fa-2x"></i>
+              <input
+                type="text"
+                placeholder="Linkedin URL"
+                name="linkedin"
+                value={linkedin}
+                onChange={e => onChange(e)}
+              />
+            </div>
+
+            <div className="form-group social-input">
+              <i className="fab fa-instagram fa-2x"></i>
+              <input
+                type="text"
+                placeholder="Instagram URL"
+                name="instagram"
+                value={instagram}
+                onChange={e => onChange(e)}
+              />
+            </div>
+          </>
+        )}
+
+        <input type="submit" className="btn btn-primary my-1" />
+        <a className="btn btn-light my-1" href="dashboard.html">
+          Go Back
+        </a>
+      </form>
+    </>
+  );
+};
+
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { createProfile }
+)(withRouter(CreateProfile));
